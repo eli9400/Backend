af317da005b44183b491b1dd47ca08fd099533d0
eli9400 <121621986+eli9400@users.noreply.github.com>
2023-07-27 11:15:26 +0300

    1

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const {
   generateUserData,
 } = require("./initialData/initialDataService");
 app.use(logger);
-/* app.use(cors); */
+app.use(cors);
 app.use(express.json());
 app.use(express.text());
 app.use(router);
